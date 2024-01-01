79e944580cc9e6ebe0a97f9f38274af22275170c
Khaled Abdsalame <154564727+Khaledabdsalame@users.noreply.github.com>
2024-01-01 02:08:25 +0100

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -2,7 +2,7 @@ import requests
 import bs4
 import random
 while True:
-    randome=input("what is type of fovorite quotes")
+    randome=input("what is The type of Your favorite quotes?")
     quotes=requests.get("https://www.goodreads.com/quotes/tag/"+randome)
     html=quotes.text
     html_perser=bs4.BeautifulSoup(html, "html.parser")
